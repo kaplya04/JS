68d8ad443a7ca90dce0319b55d4ae0de4e9b7fae
Kaplya <daniklopylov04@icloud.com>
2023-10-16 23:03:42 +0300

    mouseover

--- js/st.js
+++ js/st.js
@@ -3,3 +3,15 @@ let button = document.querySelector('.btn')
 button.addEventListener('click', function(){
     console.log('hello')
 })
+
+function setColor(e){
+    if(e.type==='mouseover')
+    e.target.style.backgroundColor = "black"
+    else 
+    if(e.type==="mouseout")
+        e.target.style.backgroundColor = "blue"
+}
+
+let click = document.querySelector('.click')
+click.addEventListener("mouseover", setColor)
+click.addEventListener("mouseout", setColor)
