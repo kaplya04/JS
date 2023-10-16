59b3563948a86fa0b17da3667b5a536f7632cd41
Kaplya <daniklopylov04@icloud.com>
2023-10-16 23:58:38 +0300

    event

--- js/st.js
+++ js/st.js
@@ -1,9 +1,11 @@
 console.log('hello')
+// click
 let button = document.querySelector('.btn')
 button.addEventListener('click', function(){
     console.log('hello')
 })
 
+// mouseover, mouseout
 function setColor(e){
     if(e.type==='mouseover')
     e.target.style.backgroundColor = "black"
@@ -16,10 +18,30 @@ let click = document.querySelector('.click')
 click.addEventListener("mouseover", setColor)
 click.addEventListener("mouseout", setColor)
 
+//mousemove
 let move = document.querySelector('#move')
-move.addEventListener('click', function(e){
+move.addEventListener('mousemove', function(e){
     console.log("screenX: " + e.screenX);
     console.log("screenY: " + e.screenY);
     console.log("clientX: " + e.clientX);
     console.log("clientY: " + e.clientY);
+})
+
+// Двойной клик
+let dva = document.querySelector('#Dblclick')
+dva.addEventListener('dblclick', function(){
+    alert('Нажал два раза')
+})
+
+// keydown, keypress, keyup 
+document.addEventListener('keydown', function(){
+    console.log('Ввод с клавиатуры')
+})
+// индентично keydown только альт и ктрл не работают
+document.addEventListener('keypress', function(){
+    console.log('Не работает с альт и ктрл')
+})
+// отжатие
+document.addEventListener('keyup', function(){
+    console.log('Отажатие')
 })
