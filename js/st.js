779a919e1133726d488ea3667cd3b08dcda2a780
Kaplya <daniklopylov04@icloud.com>
2023-10-16 23:21:28 +0300

    mousemove

--- js/st.js
+++ js/st.js
@@ -15,3 +15,11 @@ function setColor(e){
 let click = document.querySelector('.click')
 click.addEventListener("mouseover", setColor)
 click.addEventListener("mouseout", setColor)
+
+let move = document.querySelector('#move')
+move.addEventListener('click', function(e){
+    console.log("screenX: " + e.screenX);
+    console.log("screenY: " + e.screenY);
+    console.log("clientX: " + e.clientX);
+    console.log("clientY: " + e.clientY);
+})
